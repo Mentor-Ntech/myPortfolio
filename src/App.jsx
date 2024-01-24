b4f78bf22b93eae638fbe13af7aa888990248440
ameer017 <rajiabdullahi907@gmail.com>
2024-01-24 11:36:21 +0100

    update end

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import About from "./components/About/About"
-import Contact from "./components/Contact/Contact"
-import Experience from "./components/Exp/Experience"
-import Footer from "./components/Footer/Footer"
-import Header from "./components/Header/Header"
-import Hero from "./components/Hero/Hero"
+import { useEffect, useState } from "react";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Experience from "./components/Exp/Experience";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Project from "./components/Project/Project";
+import { Loader } from "./components/Loader/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <>
-      <Header/>
-      <Hero/>
-      <About/>
-      <Experience/>
-      {/* Project component here */}
-      <Contact/>
-      <Footer/>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Hero />
+          <About />
+          <Experience />
+          <Project />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
